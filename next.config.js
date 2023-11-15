b4914857b87c5245155e727104e7517d958fef94
ach02raf <ach02raf@gmail.com>
2023-11-15 20:30:08 +0100

    15-11-2023

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   output: "export",
   distDir: "docs",
-  basePath: "/docs",
-  assetPrefix: isProd ? "https://ach02raf.com" : undefined,
   reactStrictMode: true,
 };
 
