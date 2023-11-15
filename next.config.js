41ad87023477b785ac3310d94b2867982a10b683
ach02raf <ach02raf@gmail.com>
2023-11-15 22:08:25 +0100

    15-11-2023

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   output: "export",
   distDir: "docs",
   reactStrictMode: true,
+  images: {
+    domains: ["ach02raf.com"],
+    loader: "akamai",
+    path: "/",
+  },
 };
 
 module.exports = nextConfig;
