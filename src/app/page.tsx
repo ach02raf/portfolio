d2566ed5b0ded2abc65b9cf99c27e415692fe9ab
ach02raf <ach02raf@gmail.com>
2023-11-15 10:35:34 +0100

    15-11-2023

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
-import "./globals.scss";
+import Loading from "@/C/Loading/Loading";
+import "./page.scss";
 export default function Home() {
   return (
     <main>
-      <div className="loader"></div>
+      <div className="container-fluid gx-0 d-flex justify-content-center align-items-center vh-100 contain">
+        <Loading />
+      </div>
     </main>
   );
 }
---
+++ src/component/Loading/Loading.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import "./Loading.scss";
+
+function Loading() {
+  const [loading, setLoading] = useState(false);
+  const route = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(true);
+    }, 1500);
+    if (loading === true) {
+      route.push("/fr");
+    }
+  }, [loading]);
+  return <div className="loader"></div>;
+}
+export default Loading;
