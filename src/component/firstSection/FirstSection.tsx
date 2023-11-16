28802491dda00c4c230441e02b0633b256f21bcb
ach02raf <ach02raf@gmail.com>
2023-11-16 12:25:27 +0100

    16-11-2023

--- src/component/firstSection/FirstSection.tsx
+++ src/component/firstSection/FirstSection.tsx
@@ -8,8 +8,12 @@ function FirstSection(props: {
   };
 }) {
   return (
-    <div className="row p-0 m-0">
-      <div className="col-8"></div>
+    <div className="row m-0 h-100 w-100 firstSection">
+      <div className="col-8 d-block m-auto">
+        <h2>{props.firstSection.title}</h2>
+        <h2>{props.firstSection.name}</h2>
+        <h2>{props.firstSection.description}</h2>
+      </div>
       <div className="col-4"></div>
     </div>
   );
