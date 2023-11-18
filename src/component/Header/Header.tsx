4ea0ace6fbcd11242c17dbc9e42c86bd2e266bd5
safach <safa1chaabane@gmail.com>
2023-11-18 22:49:33 +0100

    logo

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import LocaleSwitcher from "../locale-switcher";
 import { useSearchParams } from "next/navigation";
 import "./Header.scss";
+import Image from "next/image";
 function Header(props: {
   header: { id: number; name: string; url: string }[];
 }) {
@@ -10,14 +11,13 @@ function Header(props: {
   return (
     <div className="sticky-top header">
       <div className="row m-0 p-0 py-3">
-        <div className="col-2">
+        <div className="col-lg-2 col-6">
           <Link href={"/"} className="text-decoration-none">
-            <h1 className="d-block m-auto header-title">
-              ACH<span>02</span>RAF
-            </h1>
+
+            <Image width={150} height={65} className="header-logo d-block m-auto" src="/Images/logohorizentalwhite1.png" title="ACH02RAF" alt="logo ach02raf" />
           </Link>
         </div>
-        <div className="col-8 d-block m-auto">
+        <div className="col-8 d-block m-auto d-lg-block d-none">
           <ul className="list-unstyled d-flex justify-content-center align-items-center p-0 m-0 ">
             {props.header.map((item) => (
               <li className="px-2" key={item.id}>
@@ -32,7 +32,7 @@ function Header(props: {
             ))}
           </ul>
         </div>
-        <div className="col-2 d-block m-auto header-div3">
+        <div className="col-lg-2 col-6 d-block m-auto header-div3">
           <LocaleSwitcher />
         </div>
       </div>
