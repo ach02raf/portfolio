c9b2d2e8a91ed0ee461bf2d02bb051ff9f37878c
ach02raf <ach02raf@gmail.com>
2023-11-19 22:08:20 +0100

    19-11-2023

--- src/component/Footer/Footer.tsx
+++ src/component/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import "./Footer.scss";
 function Footer(props: {
   Footer: {
@@ -5,10 +6,51 @@ function Footer(props: {
   };
 }) {
   return (
-    <div className="row m-0 p-0 footer">
-      <div className="col-6"></div>
-      <div className="col-6">
-        <p>{props.Footer.title}</p>
+    <div className="row m-0  footer">
+      <div className="col-lg-6 d-flex align-items-center justify-content-center">
+        <Image
+          src="/Images/Icons/linkedin.png"
+          width={20}
+          height={20}
+          alt="linkedin"
+          title="linkedin"
+          className="m-2"
+        />
+        <Image
+          src="/Images/Icons/facebook.png"
+          width={20}
+          height={20}
+          alt="facebook"
+          title="facebook"
+          className="m-2"
+        />
+        <Image
+          src="/Images/Icons/twitter.png"
+          width={20}
+          height={20}
+          alt="twitter"
+          title="twitter"
+          className="m-2"
+        />
+        <Image
+          src="/Images/Icons/github.png"
+          width={20}
+          height={20}
+          alt="github"
+          title="github"
+          className="m-2"
+        />
+        <Image
+          src="/Images/Icons/stack-overflow.png"
+          width={20}
+          height={20}
+          alt="stack-overflow"
+          title="stack-overflow"
+          className="m-2"
+        />
+      </div>
+      <div className="col-lg-6 p-2  d-flex align-items-center justify-content-center">
+        <p className="m-auto ">{props.Footer.title}</p>
       </div>
     </div>
   );
