a911597c45cb72214e1829fdc1a6b8045f317c88
ach02raf <ach02raf@gmail.com>
2023-11-20 15:19:00 +0100

    20-11-2023

--- src/component/Experience/ExperienceEducation.tsx
+++ src/component/Experience/ExperienceEducation.tsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import "./ExperienceEducation.scss"
+import React from "react";
+import "./ExperienceEducation.scss";
 function ExperienceEducation(props: {
   EducationExperience: {
     title: string;
     list: {
       title: string;
       subtitle: string;
-      desc: string;
+      desc: string[];
       date: string;
-      dessc: string[];
     }[];
-  };
+  }[];
 }) {
   return (
-    <div className='row m-auto d-block experienceEducation'>
-      <h2>{props?.EducationExperience?.title}</h2> </div>
-  )
+    <div className="row m-auto d-block experienceEducation">
+      <h2>{props.EducationExperience[0].title}</h2>
+    </div>
+  );
 }
 
-export default ExperienceEducation
+export default ExperienceEducation;
