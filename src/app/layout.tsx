212e540a85559ce2ee7dee774be72b536fa4b8c8
ach02raf <ach02raf@gmail.com>
2023-11-20 22:42:51 +0100

    20-11-2023

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.scss";
-
+import Script from "next/script";
 export const metadata: Metadata = {
   title: "ach02raf",
   description:
@@ -43,7 +43,42 @@ export default function RootLayout({
 }) {
   return (
     <html lang={params.lang}>
-      <body>{children}</body>
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-85EWZ08MHF"></script>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-85EWZ08MHF');
+        `}
+        </Script>
+        <script>
+          {`(function (w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+        var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s),
+          dl = l != "dataLayer" ? "&l=" + l : "";
+        j.async = true;
+        j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+        f.parentNode.insertBefore(j, f);
+      })(window, document, "script", "dataLayer", "GTM-WPGR2Z6D");`}
+        </script>
+      </head>
+      <body>
+        {children}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WPGR2Z6D"
+            height="0"
+            width="0"
+            className="display: none; visibility: hidden"></iframe>
+        </noscript>
+      </body>
     </html>
   );
 }
