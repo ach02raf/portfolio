4675ee45016f29f58b6eaa273b377651f27cd630
ach02raf <ach02raf@gmail.com>
2023-11-21 20:28:48 +0100

    21-11-2023

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: "export",
-  distDir: "docs",
-  reactStrictMode: true,
-  images: {
-    domains: ["ach02raf.com"],
-    path: "/",
-    unoptimized: true,
-  },
+  // output: "export",
+  // distDir: "docs",
+  // reactStrictMode: true,
+  // images: {
+  //   domains: ["ach02raf.com"],
+  //   path: "/",
+  //   unoptimized: true,
+  // },
 };
 
 module.exports = nextConfig;
