f870f59d3f12ce7baea43d510a7a2f5a14ac8aaf
ach02raf <ach02raf@gmail.com>
2023-11-23 10:20:34 +0100

    23-11-2023

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,17 +45,15 @@ export default function RootLayout({
     <html lang={params.lang}>
       <head>
         <Script id="google-analytics">
-          {`(function (w, d, s, l, i) {
-        w[l] = w[l] || [];
-        w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
-        var f = d.getElementsByTagName(s)[0],
-          j = d.createElement(s),
-          dl = l != "dataLayer" ? "&l=" + l : "";
-        j.async = true;
-        j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-        f.parentNode.insertBefore(j, f);
-      })(window, document, "script", "dataLayer", "GTM-WPGR2Z6D");`}
+          {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-7GV60J39CB');`}
         </Script>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-7GV60J39CB"></Script>
       </head>
       <body>{children}</body>
     </html>
