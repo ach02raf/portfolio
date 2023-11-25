9d6b1ed4e7b67a56a70c30a3a95a92bfc9dc4214
ach02raf <ach02raf@gmail.com>
2023-11-26 00:56:13 +0100

    26-11-2023

--- src/app/[lang]/blog/[slug]/page.tsx
+++ src/app/[lang]/blog/[slug]/page.tsx
@@ -3,7 +3,6 @@ import { getDictionary } from "../../../../../get-dictionary";
 import { Locale } from "../../../../../i18n-config";
 import { i18n } from "../../../../../i18n-config";
 import "./page.scss";
-import style from "@/app/page.module.scss";
 import Header from "@/component/Header/Header";
 import ScrollToTopButton from "@/component/ScrollToTopButton";
 import Footer from "@/component/Footer/Footer";
@@ -40,30 +39,31 @@ async function Blog({
   );
 
   return (
-    
     <div className="blog m-0">
-       <Header header={dictionary.header} />
-      
+      <Header header={dictionary.header} />
+
       <div className="blog-contain d-block mx-auto py-4">
-      <div className="blog-contain-img position-relative" >
-      <Image src={`/Images/${blogContent?.img}.jpg`}
-      alt="blog"
-      title={blogContent?.titleImg}
-      width={1920}
-      height={600} />
-       </div>
-      <h1 className="py-3 text-center">{blogContent?.title}</h1>
-     
-      {blogContent?.desc?.map((item,index)=>(
-        <div key={index}>
-          <h2>{item?.title}</h2>
-          <p>{item?.description}</p>
+        <div className="blog-contain-img position-relative">
+          <Image
+            src={`/Images/${blogContent?.img}.jpg`}
+            alt="blog"
+            title={blogContent?.titleImg}
+            width={1920}
+            height={600}
+          />
+        </div>
+        <h1 className="py-3 text-center">{blogContent?.title}</h1>
+
+        {blogContent?.desc?.map((item, index) => (
+          <div key={index}>
+            <h2>{item?.title}</h2>
+            <p>{item?.description}</p>
           </div>
-      ))}
-     </div>
-     <Footer Footer={dictionary.Footer} />
-    <ScrollToTopButton />
-     </div>
+        ))}
+      </div>
+      <Footer Footer={dictionary.Footer} />
+      <ScrollToTopButton />
+    </div>
   );
 }
 
