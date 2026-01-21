3790602d52dd905f6e3ec4373cd3640504b7dfbf
ach02raf <ach02raf@gmail.com>
2026-01-21 14:31:58 +0100

    Update Google Analytics ID and modify meta tags in layout and index files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import Script from "next/script";
 import ThemeWrapper from "@/component/ThemeWrapper";
 import AnalyticsProvider from "@/component/AnalyticsProvider";
 
-const GA_ID = "G-G67M26FKKV";
+const GA_ID = "G-905040Q7WL";
 
 export const metadata: Metadata = {
   title: "ach02raf - Full Stack Developer | Portfolio",
